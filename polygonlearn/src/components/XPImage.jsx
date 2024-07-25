d7fbbd9af51b1e454bad0dc5aa6c997f3e040c6c
XenonPydev <134782258+XenonPy@users.noreply.github.com>
2024-07-25 16:52:23 -0700

    fix xp image calculation formula

--- polygonlearn/src/components/XPImage.jsx
+++ polygonlearn/src/components/XPImage.jsx
@@ -1,18 +1,23 @@
-function findImageNumber(n) { 
-    let offset = Math.max(Math.floor(n / 10) - 4, 0);
-    let base = Math.floor(n / 10) * 10;
-    console.log(`Base: ${base}  Offset: ${offset}`);
-    return base + offset;
+function findImageNumber(n){
+  let total = 40
+  let baseIncrease = 11
+  if (n <= 50){ 
+      console.log(Math.min(Math.floor(n/10) * 10, 40))
+      return Math.min(Math.floor(n/10) * 10, 40)
+
+  }
+  while (n >= total + baseIncrease ){
+      total += baseIncrease
+      baseIncrease ++;
+  }
+  console.log(total)
+  return total;
+ 
 }
 
 
-// export const XPImage = ({num}) => {
-//     return (
-//       <img src={`/images/xpImages/xpImage${findImageNumber(Number(num))}.png`} className="xpImage"></img>
-//     )
-// }
 export const XPImage = ({num}) => {
     return (
-      <img src={`/images/xpImages/xpImage${num}.png`} className="xpImage"></img>
+      <img src={`/images/xpImages/xpImage${findImageNumber(Number(num))}.png`} className="xpImage"></img>
     )
 }
